33a65bab0eabe6d57e3b6d7bab3870d01a0d901b
Sigmaix Tech <sigmaix.tech@gmail.com>
2026-03-22 20:31:37 +0530

    robots.ts changes

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://www.sigmaix.tech/sitemap.xml",
+    sitemap: "https://www.sigmaix.io/sitemap.xml",
   };
 }
